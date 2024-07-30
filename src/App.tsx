ca90fb74f1203122f125ba677bb7c3a4b3d79142
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-30 11:54:23 +0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProjectSideBar from "./components/ProjectSideBar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 const App: React.FC = () => {
   return (
     <div className=" h-screen my-8 flex gap-8 font-mono">
       <ProjectSideBar />
+      <NoProjectSelected/>
     </div>
   );
 };
--- src/components/NoProjectSelected.tsx
+++ src/components/NoProjectSelected.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Image from "../assets/Noimage.png"
+import { Button } from './ui/button'
 
 const NoProjectSelected : React.FC = () => {
   return (
-    <div className="">
-        <img src={Image} alt="Img" className=''/>
+    <div className=" mt-44 text-center w-2/3">
+        <img src={Image} alt="Img" className=' w-16 h-16 object-contain mx-auto'/>
+        <h2 className='text-xl font-bold text-stone-500 my-4'>No Project Selected</h2>
+        <p className='text-stone-400 mb-4'>Select a project or start with a new one</p>
+        <Button>Create New Project</Button>
     </div>
   )
 }
