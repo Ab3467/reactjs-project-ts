6d08775898a1bf7aa2b540b1cbd6b689309a58a7
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-30 13:46:03 +0500

    changes

--- src/components/ProjectSideBar.tsx
+++ src/components/ProjectSideBar.tsx
@@ -15,13 +15,12 @@ export default function ProjectSideBar({
   selectedProId,
 }: ProSideBarProps) {
   return (
-    <aside className="w-64 bg-stone-200 p-4">
-        <h2 className="text-xl font-bold text-stone-500 my-4">Your Projects</h2>
+    <div className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+        <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
       <Button
-        className="w-full py-2 bg-stone-300 text-stone-700 hover:bg-stone-400"
         onClick={onStartAddProject}
       >
-        Add New Project
+        + Add Project
       </Button>
       <ul className="mt-4">
         {projects.map((project) => (
@@ -34,6 +33,6 @@ export default function ProjectSideBar({
           </li>
         ))}
       </ul>
-    </aside>
+    </div>
   );
 }
