a3baef0509e5ab49b413b0e0d7f0fef1e4dcc83e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-31 16:54:25 +0500

    Changes

--- src/components/NewProject.tsx
+++ src/components/NewProject.tsx
@@ -101,7 +101,7 @@ const NewProject: React.FC<NewProjectProps> = ({ onAdd, onCancel }) => {
             <textarea
               id="description"
               ref={Description}
-              className="w-full px-4 py-2 rounded-md bg-stone-100 text-stone-800"
+              className="w-full px-4 py-2 rounded-md bg-stone-100 text-stone-800 resize-none"
             />
           </div>
           <div className="mb-4">
