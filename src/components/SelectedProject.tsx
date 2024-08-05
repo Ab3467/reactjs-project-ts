73789e2c9f08599b9c59422690a723307ee4129c
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-05 14:57:53 +0500

    changes

--- src/components/SelectedProject.tsx
+++ src/components/SelectedProject.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Tasks from "./Tasks";
+import Tasks from "./ClearedTasks";
 import { Button } from "./ui/button";
 import { Project, Task as TaskType } from "./Types";
 import Modal from "./Modal";
