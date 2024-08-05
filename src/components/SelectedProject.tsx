76356f9ecf113e4db1a72c1942eae182a1b1a64c
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-05 12:33:53 +0500

    changes

--- src/components/SelectedProject.tsx
+++ src/components/SelectedProject.tsx
@@ -11,10 +11,10 @@ type SelectedProProps = {
   onAddTask: (text: string) => void;
   onDeleteTask: (id: number) => void;
   tasks: TaskType[];
-}
+};
 
 const SelectedPro: React.FC<SelectedProProps> = ({
- project,
+  project,
   onDeleteProject,
   onAddTask,
   onDeleteTask,
@@ -24,7 +24,7 @@ const SelectedPro: React.FC<SelectedProProps> = ({
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleAddTask = (e: React.FormEvent) => {
-      e.preventDefault();
+    e.preventDefault();
 
     const taskText = taskInputRef.current?.value ?? "";
     if (taskText.trim() === "") {
@@ -49,13 +49,15 @@ const SelectedPro: React.FC<SelectedProProps> = ({
     <div className="w-2/3 h-full bg-stone-50 px-12 py-16 rounded-xl">
       <div className="flex items-center gap-4">
         <h2 className="font-bold text-stone-500 text-lg">{project.title}</h2>
-        <Button onClick={handleDeleteConfirmation} variant="secondary">Delete Project</Button>
+        <Button onClick={handleDeleteConfirmation} variant="secondary">
+          Delete Project
+        </Button>
       </div>
       <p className="text-stone-400 my-2">{project.description}</p>
       <p className="text-stone-200 text-sm">{project.duedate}</p>
 
       {/* Thematic Break */}
-      <hr className="my-6 border-0 h-px bg-stone-300" /> 
+      <hr className="my-6 border-0 h-px bg-stone-300" />
 
       <form onSubmit={handleAddTask} className="flex gap-2">
         <Input
@@ -64,7 +66,9 @@ const SelectedPro: React.FC<SelectedProProps> = ({
           placeholder="Enter new task"
           className="w-full py-2 px-4 rounded-md bg-stone-100 text-stone-800"
         />
-        <Button type="submit" variant="ghost">Add</Button>
+        <Button type="submit" variant="ghost">
+          Add
+        </Button>
       </form>
 
       <div className="mt-8 h-96 overflow-y-auto">
